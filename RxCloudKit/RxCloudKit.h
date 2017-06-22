3f0368f1c8215b13ebb1176c2215983840d53634
Maxim Volgin <maxim.volgin@me.com>
2017-06-22 18:36:46 +0200

    initial

---
+++ RxCloudKit/RxCloudKit.h
@@ -0,0 +1,19 @@
+//
+//  RxCloudKit.h
+//  RxCloudKit
+//
+//  Created by Maxim Volgin on 22/06/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCloudKit.
+FOUNDATION_EXPORT double RxCloudKitVersionNumber;
+
+//! Project version string for RxCloudKit.
+FOUNDATION_EXPORT const unsigned char RxCloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCloudKit/PublicHeader.h>
+
+
