84d270d494f337ec6bd0a506dd55f7014797ab91
Maxim Volgin <maxim.volgin@klm.com>
2017-08-11 20:14:45 +0200

    defaults fix

--- RxCloudKit/Cache.swift
+++ RxCloudKit/Cache.swift
@@ -9,8 +9,6 @@
 import RxSwift
 import CloudKit
 
-typealias ZoneTokenMap = [Data: Data]
-
 public protocol CacheDelegate {
     func cache(record: CKRecord)
     func deleteCache(for recordID: CKRecordID)
@@ -26,13 +24,14 @@ public final class Cache {
     static let sharedTokenKey = "\(appName).sharedDatabaseTokenKey"
     static let zoneTokenMapKey = "\(appName).zoneTokenMapKey"
 
-    public let defaults = UserDefaults.standard
     public let cloud = Cloud()
     public let zoneIDs: [String]
 
+    private let token = Token()
     private let delegate: CacheDelegate
     private let disposeBag = DisposeBag()
     private var cachedZoneIDs: [CKRecordZoneID] = []
+    private var missingZoneIDs: [CKRecordZoneID] = []
 
     public init(delegate: CacheDelegate, zoneIDs: [String]) {
         self.delegate = delegate
@@ -51,6 +50,7 @@ public final class Cache {
                     print("\(zone)")
                 case .error(let error):
                     print("Error: ", error)
+                    self.missingZoneIDs.append(zoneID) // TODO
                 }
             }.disposed(by: disposeBag)
 
@@ -98,7 +98,7 @@ public final class Cache {
     }
 
     public func fetchDatabaseChanges(fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let token = self.token(for: Cache.privateTokenKey)
+        let token = self.token.token(for: Cache.privateTokenKey)
         cloud.privateDB.rx.fetchChanges(previousServerChangeToken: token).subscribe { event in
             switch event {
             case .next(let zoneEvent):
@@ -113,7 +113,7 @@ public final class Cache {
                     self.delegate.deleteCache(in: zoneID)
                 case .token(let token):
                     print("token: \(token)")
-                    self.save(token: token, for: Cache.privateTokenKey)
+                    self.token.save(token: token, for: Cache.privateTokenKey)
                     self.processAndPurgeCachedZones(fetchCompletionHandler: completionHandler)
                 }
                 
@@ -133,20 +133,13 @@ public final class Cache {
     public func fetchZoneChanges(recordZoneIDs: [CKRecordZoneID], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         var optionsByRecordZoneID: [CKRecordZoneID: CKFetchRecordZoneChangesOptions] = [:]
 
-        if let tokenMap = defaults.object(forKey: Cache.zoneTokenMapKey) as? ZoneTokenMap {
-            
-            for recordZoneID in recordZoneIDs {
-                
-                if let token = tokenMap[self.key(zoneID: recordZoneID)] {
-                    let options = CKFetchRecordZoneChangesOptions()
-                    options.previousServerChangeToken = self.value(token: token)
-                    optionsByRecordZoneID[recordZoneID] = options
-                }
-                
+        let tokenMap = self.token.zoneTokenMap(for: Cache.zoneTokenMapKey)
+        for recordZoneID in recordZoneIDs {
+            if let token = tokenMap[recordZoneID] {
+                let options = CKFetchRecordZoneChangesOptions()
+                options.previousServerChangeToken = token
+                optionsByRecordZoneID[recordZoneID] = options
             }
-            
-        } else {
-            defaults.set(ZoneTokenMap(), forKey: Cache.zoneTokenMapKey)
         }
 
         cloud.privateDB.rx.fetchChanges(recordZoneIDs: recordZoneIDs, optionsByRecordZoneID: optionsByRecordZoneID).subscribe { event in
@@ -163,7 +156,7 @@ public final class Cache {
                     self.delegate.deleteCache(for: recordID)
                 case .token(let (zoneID, token)):
                     print("token: \(zoneID)->\(token)")
-                    self.save(zoneID: zoneID, token: token, for: Cache.zoneTokenMapKey)
+                    self.token.save(zoneID: zoneID, token: token, for: Cache.zoneTokenMapKey)
                 }
                 
             case .error(let error):
@@ -183,43 +176,6 @@ public final class Cache {
         let recordZoneIDs = self.cachedZoneIDs
         self.cachedZoneIDs = []
         self.fetchZoneChanges(recordZoneIDs: recordZoneIDs, fetchCompletionHandler: completionHandler)
-    }
-
-
-    // MARK:- token
-    
-    private func save(token: CKServerChangeToken, for key: String) {
-        self.defaults.set(self.value(token: token), forKey: key)
-    }
-    
-    private func token(for key: String) -> CKServerChangeToken? {
-        
-        if let object = self.defaults.object(forKey: key) as? NSData {
-            return self.value(token: object as Data)
-        }
-        
-        return nil
-    }
-    
-    private func save(zoneID: CKRecordZoneID, token: CKServerChangeToken, for key: String) {
-        let aZoneID = self.key(zoneID: zoneID) as Data
-        let aToken = self.value(token: token)
-        if var tokenMap = self.defaults.object(forKey: key) as? ZoneTokenMap {
-            tokenMap[aZoneID] = aToken
-            self.defaults.set(tokenMap, forKey: Cache.zoneTokenMapKey)
-        }
-    }
-    
-    private func key(zoneID: CKRecordZoneID) -> Data {
-        return NSKeyedArchiver.archivedData(withRootObject: zoneID)
-    }
-    
-    private func value(token: CKServerChangeToken) -> Data {
-        return NSKeyedArchiver.archivedData(withRootObject: token)
-    }
-    
-    private func value(token: Data) ->  CKServerChangeToken? {
-        return NSKeyedUnarchiver.unarchiveObject(with: token) as? CKServerChangeToken
     }
     
 }
---
+++ RxCloudKit/Token.swift
@@ -0,0 +1,68 @@
+//
+//  Token.swift
+//  RxCloudKit
+//
+//  Created by Maxim Volgin on 11/08/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+typealias ZoneTokenMap = [CKRecordZoneID: CKServerChangeToken]
+
+class Token {
+    
+    private let defaults = UserDefaults.standard
+    
+    // MARK:- public
+    
+    public func save(token: CKServerChangeToken, for key: String) {
+        self.defaults.set(self.value(token: token), forKey: key)
+    }
+    
+    public func token(for key: String) -> CKServerChangeToken? {
+        if let object = self.defaults.object(forKey: key) as? NSData {
+            return self.value(token: object as Data)
+        }
+        return nil
+    }
+    
+    public func zoneTokenMap(for key: String) -> ZoneTokenMap {
+        if let data = self.data(for: key) {
+            return NSKeyedUnarchiver.unarchiveObject(with: data) as? ZoneTokenMap ?? ZoneTokenMap()
+        }
+        return ZoneTokenMap()
+    }
+
+    public func save(zoneID: CKRecordZoneID, token: CKServerChangeToken, for key: String) {
+        var zoneTokenMap = self.zoneTokenMap(for: key)
+        zoneTokenMap[zoneID] = token
+        self.save(zoneTokenMap: zoneTokenMap, for: key)
+    }
+    
+    // MARK:- private
+    
+    private func data(for key: String) -> Data? {
+        return self.defaults.object(forKey: key) as? Data
+    }
+    
+    private func save(zoneTokenMap: ZoneTokenMap, for key: String) {
+        let data = NSKeyedArchiver.archivedData(withRootObject: zoneTokenMap)
+        self.defaults.set(data, forKey: key)
+    }
+    
+    private func key(zoneID: CKRecordZoneID) -> Data {
+        return NSKeyedArchiver.archivedData(withRootObject: zoneID)
+    }
+    
+    private func value(token: CKServerChangeToken) -> Data {
+        return NSKeyedArchiver.archivedData(withRootObject: token)
+    }
+    
+    private func value(token: Data) ->  CKServerChangeToken? {
+        return NSKeyedUnarchiver.unarchiveObject(with: token) as? CKServerChangeToken
+    }
+    
+}
+
