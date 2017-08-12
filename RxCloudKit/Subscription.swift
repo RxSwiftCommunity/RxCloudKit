ec160c7cee3c9a32cc6c55fd01eb2ad8fdbbce79
Maxim Volgin <maxim.volgin@klm.com>
2017-08-12 08:32:58 +0200

    record sugar

---
+++ RxCloudKit/Subscription.swift
@@ -0,0 +1,15 @@
+//
+//  Subscription.swift
+//  RxCloudKit
+//
+//  Created by Maxim Volgin on 12/08/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+import CloudKit
+
+/*public*/ protocol RxCKSubscription {
+}
+
+/*public*/ extension RxCKSubscription {
+}
